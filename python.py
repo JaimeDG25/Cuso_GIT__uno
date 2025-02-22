5b0042d415743db5ab33efe6e1fe9874a775635c
Jaime Jhair DG <114628560+JaimeDG25@users.noreply.github.com>
2025-02-22 18:13:03 -0500

    Suma mejorada python.py

--- python.py
+++ python.py
@@ -1,12 +1,3 @@
-
-def sumar(a,b,c=0,d=0):
-    if c!= 0 and d!=0 :
-        return a+b+c+d
-    if c!= 0 and d==0:
-        return a+b+c
-    if d!=0 and c==0:
-        return a+b+d
-    else:
-        return a+b
-print ("Un saludo")
-print(sumar(1,2,3,4,5))
+def sumar_mejorada(*suma):
+    return sum(suma)
+print(sumar_mejorada(1,2,3,4,5,6))
